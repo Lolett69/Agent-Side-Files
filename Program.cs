3fbaf5e0b657f08379bcb4f4006d03da2a6dc5ab
Lolett69 <lolett69@gmail.com>
2026-03-23 15:18:20 +0800

    Add FAQ model, DbContext, HelpCenter summary box, and backend setup

--- Program.cs
+++ Program.cs
@@ -1,27 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using NextHorizon.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
+// Connect to database
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Agent/HelpCenter");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Agent}/{action=HelpCenter}/{id?}")
     .WithStaticAssets();
-
 
 app.Run();
